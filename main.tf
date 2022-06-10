45e006b89f8211cec1f0a2a3709cf7d1988dc17a
olicodema <servicesoliviaa@gmail.com>
2022-06-11 00:32:48 +0100

    acc name fix

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_linux_function_app" "functionapp-weu" {
 }
 
 resource "azurerm_storage_account" "fun-weu-storage" {
-  name = "linuxfunctionappstorageweu"
+  name = "linuxfuncappstrgeweu001"
   resource_group_name = azurerm_resource_group.github-actions-tf.name
   location = azurerm_resource_group.github-actions-tf.location
   account_tier = "Standard"
